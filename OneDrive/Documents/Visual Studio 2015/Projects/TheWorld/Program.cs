f2c7fdb873f30f16b224876968ab453821d237b4
manognya <koduganti.m@husky.neu.edu>
2017-02-23 19:14:16 -0500

    till html pages

---
+++ OneDrive/Documents/Visual Studio 2015/Projects/TheWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace TheWorld
+{
+    public class Program
+    {
+        // run starts here
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                // webserver
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                //for headers
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
